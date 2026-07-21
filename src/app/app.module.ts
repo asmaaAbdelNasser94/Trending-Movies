a0430097632ef9acd522a999e3244aa5b8446fad
Asmaa A. Mohamed <as.farag@hollat.net>
2026-07-22 02:44:45 +0300

    Update Angular V 17 -> 18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,7 @@ import { NotFoundComponent } from './not-found/not-found.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { MoviesDetailsComponent } from './movies-details/movies-details.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import {HttpClient, HttpClientModule} from '@angular/common/http';
+import { HttpClient, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { MovieDetailesComponent } from './movie-detailes/movie-detailes.component';
 import { TvDetailsComponent } from './tv-details/tv-details.component' ;
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -30,51 +30,44 @@ import { SearchComponent } from './search/search.component';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    AboutComponent,
-    ContactsComponent,
-    MoviesComponent,
-    LoginComponent,
-    RegisterComponent,
-    NotFoundComponent,
-    NavbarComponent,
-    MoviesDetailsComponent,
-    MovieDetailesComponent,
-    TvDetailsComponent,
-    HeaderComponent,
-    NotificationsComponent,
-    PrivacyComponent,
-    FooterComponent,
-    TvComponent,
-    SeeMorePipe,
-    SearchComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule ,
-    ReactiveFormsModule ,
-    HttpClientModule ,
-    BrowserAnimationsModule ,
-    CarouselModule ,
-    NgxPaginationModule ,
-    NgxHttpLoaderModule.forRoot() ,
-    SweetAlert2Module.forRoot() ,
-    FormsModule ,
-    TranslateModule.forRoot({
-      defaultLanguage : 'en' ,
-      loader: {
-        provide: TranslateLoader,
-        useFactory: HttpLoaderFactory,
-        deps: [HttpClient]
-      }
-    })
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        HomeComponent,
+        AboutComponent,
+        ContactsComponent,
+        MoviesComponent,
+        LoginComponent,
+        RegisterComponent,
+        NotFoundComponent,
+        NavbarComponent,
+        MoviesDetailsComponent,
+        MovieDetailesComponent,
+        TvDetailsComponent,
+        HeaderComponent,
+        NotificationsComponent,
+        PrivacyComponent,
+        FooterComponent,
+        TvComponent,
+        SeeMorePipe,
+        SearchComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        ReactiveFormsModule,
+        BrowserAnimationsModule,
+        CarouselModule,
+        NgxPaginationModule,
+        NgxHttpLoaderModule.forRoot(),
+        SweetAlert2Module.forRoot(),
+        FormsModule,
+        TranslateModule.forRoot({
+            defaultLanguage: 'en',
+            loader: {
+                provide: TranslateLoader,
+                useFactory: HttpLoaderFactory,
+                deps: [HttpClient]
+            }
+        })], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
 export function HttpLoaderFactory(_httpClient : HttpClient){
   return new TranslateHttpLoader(_httpClient, './assets/i18n/', '.json');
